48e4ac3e35f12beaa7fa370eb1ac9c30484fd460
lee-yeon-ji <duswl2418@naver.com>
2025-10-15 23:18:48 +0900

    테스트

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+function Header() {
+    return (
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Project</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
