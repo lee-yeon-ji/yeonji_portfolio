6f480575b1b4ef5173d357d7ffcafe6f485f0d2e
lee-yeon-ji <duswl2418@naver.com>
2025-10-18 22:16:20 +0900

    project page 틀 작업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,48 +15,51 @@ export default function Header() {
     const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
     return (
-        <header className="sticky top-0 bg-gray-300 position-fixed w-full left-0 rigth-0 z-999 ">
-            <div className="flex justify-between items-center mx-auto w-11/12 h-16 font-bold text-base">
-                <Link to='/'>yeonji</Link>
-                <nav className="items-center space-x-6 hidden sm:flex">
-                    {navItems.map(item => (
-                        <NavLink key={item.id} to={item.to} className="hover:text-white">
-                            {item.label}
-                        </NavLink>
+        <div className="sticky top-0 bg-gray-300 position-fixed w-full left-0 rigth-0 z-999  ">
+            <header className="max-w-5xl mx-auto">
+                <div className="flex justify-between items-center w-full h-16 font-bold text-base px-4">
+                    <Link to='/'>yeonji</Link>
+                    <nav className="items-center space-x-6 hidden sm:flex">
+                        {navItems.map(item => (
+                            <NavLink key={item.id} to={item.to} className="hover:text-white">
+                                {item.label}
+                            </NavLink>
 
-                    ))}
+                        ))}
 
-                </nav>
-                <button className="sm:hidden" onClick={toggleMenu}>
-                    <FaBars />
-                </button>
-            </div>
-
-        {/* mobile Menu */}
-            <aside className={`
-            fixed top-0 right-0 w-64 h-full bg-gray-300 z-999
-            ${isMenuOpen ? '-translate-x-0' : 'translate-x-full'}
-            sm:hidden transform transition-transform duration-300 ease-in-out`} >
-
-                <div className="flex justify-end p-4">
-                    <button className="text-white focus:outline-none" aria-label="close menu" onClick={toggleMenu}>
-                        <FaTimes className="h-6 w-6" />
+                    </nav>
+                    <button className="sm:hidden" onClick={toggleMenu}>
+                        <FaBars />
                     </button>
                 </div>
-                <nav className="flex flex-col space-y-4 p-4 items-start w-full h-screen pt-32">
-                    {navItems.map(item => (
-                        <NavLink key={item.id} to={item.to} className="hover:text-white">
-                            {item.label}
-                        </NavLink>
 
-                    ))}
-                </nav>
+                {/* mobile Menu */}
+                <aside className={`
+            fixed top-0 right-0 w-64 h-full bg-gray-300 z-999 
+            ${isMenuOpen ? '-translate-x-0' : 'translate-x-full'}
+            sm:hidden transform transition-transform duration-300 ease-in-out `} >
+
+                    <div className="flex justify-end p-4">
+                        <button className="text-white focus:outline-none" aria-label="close menu" onClick={toggleMenu}>
+                            <FaTimes className="h-6 w-6" />
+                        </button>
+                    </div>
+                    <nav className="flex flex-col space-y-4 p-4 items-start w-full h-screen pt-32">
+                        {navItems.map(item => (
+                            <NavLink key={item.id} to={item.to} className="hover:text-white">
+                                {item.label}
+                            </NavLink>
+
+                        ))}
+                    </nav>
 
 
 
-            </aside>
+                </aside>
 
 
-        </header>
+            </header>
+
+        </div>
     );
 };
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -11,28 +11,37 @@ export default function Project() {
 
     }
 
+    const ProjectCard = () => {
+        return (
+            <div className="">
+                <span className="">콘텐츠</span>
+            </div>
+        )
+    }
+
     return (
-        <div>
+        <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
 
 
             <div className="">
-                <h2 className="my-8  text-4xl md:text-5xl leading-10	sm:leading-12 lg:text-6xl"> Project</h2>
+                <h2 className="my-8 text-4xl md:text-5xl leading-10 text-center font-bold	sm:leading-12 lg:text-6xl">My Most<br/> Recent Project</h2>
             </div>
 
-            <div className=" w-fit flex gap-2 items-center justify-center rounded-full px-4 py-3 bg-gray-300 max-sm:w-full">
-
-
-                <ProjectButton name={"ALL"}/>
-                <ProjectButton name={"JS"}/>
-                <ProjectButton name={"REACT"}/>
-
-
+            <div className="flex justify-center">
+                <div className=" w-fit flex gap-2 items-center justify-center rounded-full px-4 py-3 bg-gray-300 max-sm:w-full">
+                    <ProjectButton name={"ALL"}/>
+                    <ProjectButton name={"JS"}/>
+                    <ProjectButton name={"REACT"}/>
+                </div>
             </div>
+
 
             <div className="">
                 <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                    <li >
-                        <div className=""></div>
+                    <li>
+                        <div className="border-3 border-gray-300 rounded-lg p-4">
+                            <span className="">콘텐츠</span>
+                        </div>
                     </li>
                 </ul>
             </div>
