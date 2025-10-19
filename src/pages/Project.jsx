40bcbe8281485814cd4374b8711b4c04e766eccd
lee-yeon-ji <duswl2418@naver.com>
2025-10-19 22:51:19 +0900

    project page 틀 작업

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -36,10 +36,15 @@ export default function Project() {
             </div>
 
 
-            <div className="">
-                <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="pt-14">
+                <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 ">
+                    <li >
+                        <div className="border-2 border-slate-500 rounded-lg p-4 h-64 ">
+                            <span className="">콘텐츠</span>
+                        </div>
+                    </li>
                     <li>
-                        <div className="border-3 border-gray-300 rounded-lg p-4">
+                        <div className="border-2 border-slate-500 rounded-lg p-4 h-64  ">
                             <span className="">콘텐츠</span>
                         </div>
                     </li>
