72adf84ec7f516d054e3cf8cd697252853fa96b7
lee-yeon-ji <duswl2418@naver.com>
2025-10-23 23:24:58 +0900

    project page about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,33 +2,43 @@ import { motion, AnimatePresence } from "framer-motion";
 
 export default function About() {
     return (
-        <div className="opacity-1">
-            <div>
-                <div className="font-bold">about me</div>
-                <div className="pt-6">
-                    안녕하세요 저는 프론트엔드 개발자가 되고 싶은 이연지입니다.<br/> 시간을 내어 제 포트폴리오를 봐주셔서 감사합니다.
+        <div className="flex opacity-1">
+            <div className="">
+                <div className="max-w-2xl   mx-auto">
+                    <img src="/img/img_portfolio.png" alt="img"/>
                 </div>
             </div>
 
-            <div>
-                <div className="font-bold">certificate</div>
-                <div className="pt-6">
-                    정보처리기사(2025) - 실기 시험예정
+            <div className="flex-1 min-w-0 pt-14">
+                <div >
+                    <div className="font-bold text-3xl">about me</div>
+                    <div className="pt-6">
+                        안녕하세요 저는 프론트엔드 개발자가 되고 싶은 이연지입니다.<br/> 시간을 내어 제 포트폴리오를 봐주셔서 감사합니다.
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div className="font-bold">My tech stack</div>
-                <div className="pt-6">
-                    <ul>
-                        <li>
 
-                        </li>
-
-                        <li></li>
-                    </ul>
-
+                <div className="pt-14">
+                    <div className="font-bold text-3xl">certificate</div>
+                    <div className="pt-6">
+                        정보처리기사(2025) - 실기 시험예정
+                    </div>
                 </div>
+                <div className="pt-14">
+                    <div className="font-bold text-3xl">My tech stack</div>
+                    <div className="pt-6">
+                        <ul>
+                            <li>
+
+                            </li>
+
+                            <li></li>
+                        </ul>
+
+                    </div>
+                </div>
+
             </div>
+
 
         </div>
     );
