d5046f9cafab2734f8254577dc9b28585d3c27e9
lee-yeon-ji <duswl2418@naver.com>
2025-10-30 00:08:38 +0900

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,17 @@
 export default function Home() {
     return (
         <div>
-            <h1></h1>
+            <h1>LEE YEON JI</h1>
+            <div className={""}>
+                <div>Creative</div>
+                <div>developer</div>
+            </div>
+
+            <div className="">
+                <a className="" href="/project">go project!</a>
+
+            </div>
+
         </div>
     );
 };
