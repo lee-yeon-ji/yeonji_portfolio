3a247dd8ba9acf6bfef77dd05c5efc7040e300c0
lee-yeon-ji <duswl2418@naver.com>
2025-10-31 00:07:08 +0900

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,27 @@
 
 export default function Home() {
     return (
-        <div>
-            <h1 className="font-bold">LEE YEON JI</h1>
-            <div className={""}>
-                <div>Creative</div>
-                <div>developer</div>
+
+
+        <div className="grid grid-cols-2 gap-10 ">
+            <div>
+                <div className="">이미지</div>
             </div>
+            <div>
+                <h1 className="font-bold text-8xl">LEE YEON JI</h1>
 
-            <div className="">
-                <a className="" href="/project">go project!</a>
+                <div className="flex gap-4 text-3xl ">
+                    <div>Creative</div>
+                    <div className="font-semibold">developer</div>
+                </div>
+
+                <div className="h-15">
+                    <a className="bg-gray-300  p-5" href="/project">go project!</a>
+
+                </div>
 
             </div>
-
         </div>
+
     );
 };
