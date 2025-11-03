8a948114128cfe0e7bf225102f013751c78771bb
lee-yeon-ji <duswl2418@naver.com>
2025-11-03 23:28:46 +0900

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,22 @@ export default function Home() {
     return (
 
 
-        <div className="grid grid-cols-2 gap-10 ">
+        <div className="grid grid-cols-2 gap-14 max-w-7xl mx-auto max-h-full">
             <div>
                 <div className="">
                     <img src="/img/img_home_bg.jpg" alt="img"/>
                 </div>
             </div>
-            <div>
+            <div className="flex flex-col justify-center ">
                 <h1 className="font-bold text-8xl">LEE YEON JI</h1>
 
-                <div className="flex gap-4 text-3xl ">
+                <div className="flex gap-4 text-3xl pt-14">
                     <div>Creative</div>
                     <div className="font-semibold">developer</div>
                 </div>
 
-                <div className="">
-                    <a className="inline-flex items-center bg-gray-300  p-5" href="/project">go project!</a>
+                <div className="pt-14">
+                    <a className="bg-black text-white px-4 py-4 text-3xl rounded shadow hover:shadow-lg hover:bg-white hover:text-black hover:border-black transition duration-200" href="/project">go project!</a>
 
                 </div>
 
