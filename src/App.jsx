056b8dcce2ab2ea13da28399da0a1d9591c99bb9
lee-yeon-ji <duswl2418@naver.com>
2025-12-09 23:58:52 +0900

    project popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import ThemeToggle from './components/ThemeToggle';
-import MouseMove from "./components/MouseMoving";
 
 function App() {
   return (
     <>
-      <MouseMove />
+
       <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
           <Header />
           <Outlet />
