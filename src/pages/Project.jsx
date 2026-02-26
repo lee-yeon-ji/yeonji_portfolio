45a5cdaa93dafac1b417a9021040d2cfcd5bb4dc
lee-yeon-ji <duswl2418@naver.com>
2026-02-26 23:17:24 +0900

    전반적인 ui 개선

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -239,6 +239,7 @@ export default function Project() {
                                 </motion.li>
                             ))}
                         </motion.ul>
+
                     )}
                 </AnimatePresence>
             </div>
